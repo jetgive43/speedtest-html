32b802beb8826ec62ebd54682702837adbd30443
Web and Blockchain full stack developer <142791357+GreenWorld14@users.noreply.github.com>
2023-11-20 17:32:14 +0900

    code complete

---
+++ js/custom.js
@@ -0,0 +1,67 @@
+var imageAddr =
+  "https://www.kasandbox.org/programming-images/avatars/duskpin-sapling.png";
+var downloadSize = 7300000; //bytes
+console.log("okay");
+
+function ShowProgressMessage(msg) {
+  if (console) {
+    if (typeof msg == "string") {
+      console.log(msg);
+    } else {
+      for (var i = 0; i < msg.length; i++) {
+        console.log(msg[i]);
+      }
+    }
+  }
+
+  var oProgress = document.getElementById("progress");
+  if (oProgress) {
+    var actualHTML = typeof msg == "string" ? msg : msg.join("<br />");
+    oProgress.innerHTML = actualHTML;
+  }
+}
+
+function InitiateSpeedDetection() {
+  ShowProgressMessage("Loading the image, please wait...");
+  window.setTimeout(MeasureConnectionSpeed, 1);
+}
+
+if (window.addEventListener) {
+  window.addEventListener("load", InitiateSpeedDetection, false);
+} else if (window.attachEvent) {
+  window.attachEvent("onload", InitiateSpeedDetection);
+}
+
+function MeasureConnectionSpeed() {
+  var startTime, endTime;
+  var download = new Image();
+  download.onload = function () {
+    endTime = new Date().getTime();
+    showResults();
+  };
+
+  download.onerror = function (err, msg) {
+    ShowProgressMessage("Invalid image, or error downloading");
+  };
+
+  startTime = new Date().getTime();
+  var cacheBuster = "?nnn=" + startTime;
+  download.src = imageAddr + cacheBuster;
+
+  function showResults() {
+    var duration = (endTime - startTime) / 1000;
+    var bitsLoaded = downloadSize * 8;
+    var speedBps = (bitsLoaded / duration).toFixed(2);
+    var speedKbps = (speedBps / 1024).toFixed(2);
+    var speedMbps = (speedKbps / 1024).toFixed(2);
+    document.getElementById("bps_p").innerText = speedBps+' bps';
+    document.getElementById("kbps_p").innerText = speedKbps + " bps";
+    document.getElementById("mbps_p").innerText = speedMbps + " bps";
+    ShowProgressMessage([
+      "Your connection speed is:",
+      speedBps + " bps",
+      speedKbps + " kbps",
+      speedMbps + " Mbps",
+    ]);
+  }
+}
